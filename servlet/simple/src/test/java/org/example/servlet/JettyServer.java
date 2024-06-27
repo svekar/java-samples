07f7cb4536205e03adebc0cabe75ef8464be1a7c
Sven-Jørgen Karlsen <svejk@hotmail.no>
2024-06-28 00:21:25 +0200

    Update package name of ServletContextHandler

--- servlet/simple/src/test/java/org/example/servlet/JettyServer.java
+++ servlet/simple/src/test/java/org/example/servlet/JettyServer.java
@@ -1,7 +1,7 @@
 package org.example.servlet;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 
 public class JettyServer {
 
